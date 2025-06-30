0590d026efe31a98164da643659bc471e398aba9
WardenBradley <nazmuzzaman13@gmail.com>
2025-07-01 00:26:06 +0530

    End of Section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Miguel");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Peña");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Javier");
